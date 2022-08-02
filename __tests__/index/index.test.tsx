833373a86d47e24cea401083f7f04245cbbd052d
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-02 16:12:45 +0900

    update checking lint

--- __tests__/index/index.test.tsx
+++ __tests__/index/index.test.tsx
@@ -1,11 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import {
+  render, screen 
+} from '@testing-library/react';
 import Home from '@/pages/index';
 
 describe('Home', () => {
-  it("renders a heading", () => {
-    const { container } = render(<Home />);
+  it('renders a heading', () => {
+    const {
+      container 
+    } = render(<Home />);
 
-    const heading = screen.getByRole('heading', {name: /welcome to next\.js!/i});
+    const heading = screen.getByRole('heading', {
+      name: /welcome to next\.js!/i
+    });
 
     expect(heading).toBeInTheDocument();
 
