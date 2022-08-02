316ae0ff050f94c514ef746e074ab429eb54a05b
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-02 15:49:51 +0900

    update path

--- __tests__/index/index.test.tsx
+++ __tests__/index/index.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import Home from '../../pages/index';
+import Home from '@/pages/index';
 
 describe('Home', () => {
-  it('renders a heading', () => {
+  it("renders a heading", () => {
     const { container } = render(<Home />);
 
     const heading = screen.getByRole('heading', {name: /welcome to next\.js!/i});
