13ebe34d7d0b2db9169c92344a035753f1fb736c
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-04 20:52:47 +0900

    add nested layout

---
+++ components/templates/layouts/Nested.tsx
@@ -0,0 +1,15 @@
+import { ILayoutProps } from '@/models/interfaces/props/layout';
+import Header from '@/components/organisms/Header';
+import { useScrollPosition } from '@/hooks/useScrollPosition';
+
+export default function Nested({ children }: ILayoutProps) {
+  const { scrollY } = useScrollPosition();
+
+  return (
+    <section className='min-h-screen flex flex-col'>
+      <Header scrollY={scrollY}/>
+      <main className='flex-1'>{children}</main>
+      <footer>footer</footer>
+    </section>
+  );
+}
