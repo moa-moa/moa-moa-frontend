2ec3af3a65efa120cfb7164034ae1a9bdac79568
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-04 22:54:31 +0900

    Header margin 설정

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,4 +1,4 @@
-import Icons from '@/components/icons';
+import Icons from '@/components/atoms/icons';
 
 interface HeaderProps {
   scrollY: number;
@@ -8,13 +8,10 @@ export default function Haeder({ scrollY }: HeaderProps) {
   const activeShadow = scrollY > 20 ? ' shadow-md' : '';
 
   return (
-    <header
-      className={
-        'h-header flex justify-center items-center sticky top-0 left-0' +
-        activeShadow
-      }
-    >
-      <Icons.HeaderLogo />
+    <header className={'h-header sticky top-0 left-0' + activeShadow}>
+      <section className='flex justify-center items-center max-w-7xl mx-auto w-full h-full'>
+        <Icons.HeaderLogo />
+      </section>
     </header>
   );
 }
