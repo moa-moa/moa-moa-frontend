a528d79d7e3e7a4cd7b72c8a441265063f5fae2f
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-05 01:16:37 +0900

    add home, profile page components

---
+++ components/pages/Profile.tsx
@@ -0,0 +1,9 @@
+import Layout from '@/components/templates/layouts';
+
+export default function Profile() {
+  return (
+    <Layout.Nested>
+      <h1 className='text-3xl underline'>Profile</h1>
+    </Layout.Nested>
+  );
+}
