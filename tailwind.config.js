4581bd4706bd1acb364d5f10e0cacd6adca7bf2a
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-04 20:51:42 +0900

    높이 클래스 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        'header': '3.25rem'
+      }
+    },
   },
   plugins: [],
 };
