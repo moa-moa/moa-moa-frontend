08b3ce927d81a521ddda5fed649016ff3d8b111e
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-04 20:53:08 +0900

    apply nested layout in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from 'next';
+import Layout from '@/components/templates/layouts';
 
 const Home: NextPage = () => {
-  const hello = 'hello';
-
-  if (hello === 'hello') { 
-    return <div>HEllo</div>;
-  }
-  
-  return <h1 className='text-3xl underline'>Hello world!</h1>;
+  return (
+    <Layout.Nested>
+      <h1 className='text-3xl underline'>Hello world!</h1>
+    </Layout.Nested>
+  );
 };
 
 export default Home;
