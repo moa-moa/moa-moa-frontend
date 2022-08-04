8bf5947eb3b0c50b2b3662cfef5332f5f773ee5f
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-04 23:31:26 +0900

    add footer'icons

--- components/atoms/icons/index.ts
+++ components/atoms/icons/index.ts
@@ -1,5 +1,13 @@
 import HeaderLogo from './HeaderLogo';
+import HomeOff from './HomeOff';
+import HomeOn from './HomeOn';
+import MyPageOff from './MyPageOff';
+import MyPageOn from './MyPageOn';
 
 export default {
-  HeaderLogo
+  HeaderLogo,
+  HomeOff,
+  HomeOn,
+  MyPageOff,
+  MyPageOn
 };
