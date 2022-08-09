d574bf4401ff89786619d1c797b89ff93c24aa6f
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-09 17:19:53 +0900

    add background color in Header

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -8,7 +8,7 @@ export default function Haeder({ scrollY }: HeaderProps) {
   const activeShadow = scrollY > 20 ? ' shadow-md' : '';
 
   return (
-    <header className={'h-header sticky top-0 left-0' + activeShadow}>
+    <header className={'h-header sticky top-0 left-0 bg-white' + activeShadow}>
       <section className='flex justify-center items-center max-w-7xl mx-auto w-full h-full'>
         <Icons.HeaderLogo />
       </section>
