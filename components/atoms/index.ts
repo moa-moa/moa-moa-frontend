32bb27195fc3b73efb58e745a5a5d34623ca71b2
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-09 17:18:22 +0900

    add atom manage file

---
+++ components/atoms/index.ts
@@ -0,0 +1,11 @@
+import Badge from './Badge';
+import GnbItem from './gnb/GnbItem';
+import Icons from './icons';
+import Avatar from './Avatar';
+
+export default {
+  Badge,
+  GnbItem,
+  Icons,
+  Avatar
+};
