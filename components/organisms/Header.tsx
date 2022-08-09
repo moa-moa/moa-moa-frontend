0f9dd4f20eab8d8d25e012f37a60d77e10b9061e
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-09 17:24:30 +0900

    use UseScrollPosition in client-side

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,14 +1,13 @@
+import { useScrollPosition } from '@/hooks/useScrollPosition';
 import Icons from '@/components/atoms/icons';
 
-interface HeaderProps {
-  scrollY: number;
-}
-
-export default function Haeder({ scrollY }: HeaderProps) {
+export default function Haeder() {
+  const { scrollY } = useScrollPosition();
   const activeShadow = scrollY > 20 ? ' shadow-md' : '';
 
   return (
-    <header className={'h-header sticky top-0 left-0 bg-white' + activeShadow}>
+    <header
+      className={'h-header sticky top-0 left-0 bg-white z-10' + activeShadow}>
       <section className='flex justify-center items-center max-w-7xl mx-auto w-full h-full'>
         <Icons.HeaderLogo />
       </section>
--- components/templates/layouts/Nested.tsx
+++ components/templates/layouts/Nested.tsx
@@ -1,15 +1,12 @@
 import { ILayoutProps } from '@/models/interfaces/props/layout';
-import { useScrollPosition } from '@/hooks/useScrollPosition';
 
 import Header from '@/components/organisms/Header';
 import Footer from '@/components/organisms/Footer';
 
 export default function Nested({ children }: ILayoutProps) {
-  const { scrollY } = useScrollPosition();
-
   return (
     <section className='min-h-screen flex flex-col'>
-      <Header scrollY={scrollY} />
+      <Header />
       <main className='flex-1 mt-[0.1rem] max-w-5xl w-full mx-auto'>
         {children}
       </main>
