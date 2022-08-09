f0fc74bc4b87b1a18890b5e340dae9799e4b1c69
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-09 17:20:04 +0900

    add ClubList

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,9 +1,11 @@
 import Header from './Header';
 import Footer from './Footer';
 import TabCategories from './TabCategories';
+import ClubList from './ClubList';
 
 export default {
   Header,
   Footer,
-  TabCategories
+  TabCategories,
+  ClubList
 };
