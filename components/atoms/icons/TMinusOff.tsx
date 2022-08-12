0805bd619d4e8d13ef909bebee0336a53bf4da0c
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-12 09:41:40 +0900

    add icons

---
+++ components/atoms/icons/TMinusOff.tsx
@@ -0,0 +1,12 @@
+export default function TMinusOff() {
+  return (
+    <svg
+      width='36'
+      height='36'
+      viewBox='0 0 36 36'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'>
+      <rect x='12' y='17' width='12' height='2' rx='1' fill='#AAAAAA' />
+    </svg>
+  );
+}
