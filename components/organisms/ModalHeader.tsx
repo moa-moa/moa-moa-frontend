af7bf12b57fcceaf9b9d2ef51bf171352ddf17fa
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-12 10:19:55 +0900

    update left axis in modal-header

--- components/organisms/ModalHeader.tsx
+++ components/organisms/ModalHeader.tsx
@@ -8,7 +8,7 @@ export default function ModalHeader({ title }: Props) {
   return (
     <header className='h-header sticky top-0 left-0 bg-white z-10 px-4'>
       <section className='relative max-w-5xl mx-auto w-full h-full'>
-        <button className='absolute left-0 top-1/2 -translate-y-1/2'>
+        <button className='absolute left-2 top-1/2 -translate-y-1/2'>
           <Icons.Close />
         </button>
         <h1 className='absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 text-lg text-[#222222] font-normal'>
