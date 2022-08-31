5b15fb8c7c66b1fe89bee4de5e6ada29611cf09e
taesung1993 <cheonyulin@gmail.com>
2022-09-01 00:13:33 +0900

    add useNavigationGuard

---
+++ hooks/useNavigationGuard.tsx
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
+import AuthService from 'services/auth.service';
+
+export default function useNavigationGuard() {
+  const router = useRouter();
+  const response = useQuery(['auth'], AuthService.googleLogin, {
+    retryOnMount: false,
+    refetchOnWindowFocus: false,
+    onSuccess: (data) => {
+      AuthService.setAccessToken(data.accessToken);
+    },
+    onError: (error) => {
+      router.replace('/login');
+    }
+  });
+
+  return response;
+}
