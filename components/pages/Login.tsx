7f8cebca0d970bb6458905064ea66ec3e2991abb
taesung1993 <cheonyulin@gmail.com>
2022-08-31 21:35:03 +0900

    add login button

--- components/pages/Login.tsx
+++ components/pages/Login.tsx
@@ -1,18 +1,6 @@
 import Layout from '@/components/templates';
 import Atoms from '../atoms';
 import Icons from '../icons';
-import axios from 'axios';
-
-async function login() {
-  try {
-    const res = await axios.get('/moamoa/auth/google', {
-      withCredentials: true
-    });
-    console.log(res);
-  } catch (error) {
-    console.log(error);
-  }
-}
 
 export default function Login() {
   return (
@@ -21,11 +9,7 @@ export default function Login() {
         <div className='w-[13.75rem] h-9 mb-12'>
           <Icons.Logo width='220' height='36' />
         </div>
-        <Atoms.LoginButton
-          onClick={() => {
-            login();
-          }}
-        />
+        <Atoms.LoginButton />
       </section>
     </Layout.Void>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+let flag = true;
+
+async function runAutoLogin() {
+  try {
+    const response = await axios.get('/moamoa/auth/auto-login');
+    console.log(response);
+  } catch (error: any) {
+    console.log(error);
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    if (flag) {
+      runAutoLogin();
+      flag = false;
+    }
+  }, []);
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
