ac929d58317c126906c83ffe3b275b82943b2737
taesung1993 <cheonyulin@gmail.com>
2022-09-01 18:40:10 +0900

    disable retry

--- hooks/useNavigationGuard.tsx
+++ hooks/useNavigationGuard.tsx
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
-import AuthService from 'services/auth.service';
+import AuthService from '@/services/auth.service';
 
 export default function useNavigationGuard() {
   const router = useRouter();
   const response = useQuery(['auth'], AuthService.googleLogin, {
-    retryOnMount: false,
+    retry: false,
     refetchOnWindowFocus: false,
     onSuccess: (data) => {
       AuthService.setAccessToken(data.accessToken);
