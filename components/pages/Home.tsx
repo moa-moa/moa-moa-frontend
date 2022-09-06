230cb2515bb325f2052ff9f7dcc1f6b00ca17817
taesung1993 <cheonyulin@gmail.com>
2022-09-07 01:33:32 +0900

    bind categories data in and create clubs page

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -4,14 +4,25 @@ import Atoms from '../atoms';
 import Modal from '../atoms/Modal';
 import { useRouter } from 'next/router';
 import useCategories from '@/hooks/useCategories';
+import { useState } from 'react';
 
 export default function Home() {
   const router = useRouter();
   const { data, isLoading, isError } = useCategories();
+  const [selectedCategoryId, setSelectedCategoryId] = useState<number>(-1);
 
   return (
     <Layout.Nested>
-      <Organisms.TabCategories info={{ data, isLoading, isError }} />
+      <Organisms.TabCategories
+        info={{ data, isLoading, isError }}
+        options={{
+          withAllandEnd: true,
+          selected: {
+            id: selectedCategoryId,
+            set: (id: number) => setSelectedCategoryId(id)
+          }
+        }}
+      />
       <section className='px-4'>
         <Organisms.AvailableClubs />
         <Organisms.UnAvailableClubs />
