9d00f01aec6c1cbe2c5a1e8bf3297e2014fbcd79
taesung1993 <cheonyulin@gmail.com>
2022-09-07 00:58:42 +0900

    update props

--- components/atoms/CategoryButton.tsx
+++ components/atoms/CategoryButton.tsx
@@ -1,44 +1,34 @@
 import useHover from '@/hooks/useHover';
-import { ICategory } from '@/models/interfaces/data/Category';
-import { Dispatch } from 'react';
 
 interface Props {
   info: {
     id: number;
     name: string;
     num: number;
-    selected: {
-      category: ICategory;
-      setCategory: Dispatch<ICategory>;
-    };
+    isActive: boolean;
   };
   style: { backColor: string };
+  onClick: () => void;
 }
 
 export default function CategoryButton({
-  info: { id, name, num, selected },
-  style
+  info: { name, num, isActive },
+  style,
+  onClick
 }: Props) {
   const [hoverRef, isHovered] = useHover();
   const buttonText = `${name} (${num})`;
-  const isSelected = id === selected.category.id;
   return (
     <button
       className='px-2.5 py-[0.3125rem] text-sm shrink-0 border border-[#dddddd] rounded-[0.3125rem]'
       style={{
-        backgroundColor: isSelected ? style.backColor : '#fff',
-        color: isSelected ? '#fff' : '#222222',
-        fontWeight: isSelected ? 'bold' : 'normal',
+        backgroundColor: isActive ? style.backColor : '#fff',
+        color: isActive ? '#fff' : '#222222',
+        fontWeight: isActive ? 'bold' : 'normal',
         opacity: isHovered ? 0.7 : 1
       }}
       ref={hoverRef}
-      onClick={() =>
-        selected.setCategory({
-          id: id,
-          name: name,
-          backColor: style.backColor
-        })
-      }>
+      onClick={onClick}>
       {buttonText}
     </button>
   );
--- components/atoms/tests/CategoryButton.test.tsx
+++ components/atoms/tests/CategoryButton.test.tsx
@@ -8,14 +8,7 @@ const CATEGORIES = {
       id: -1,
       name: '전체',
       num: 7,
-      selected: {
-        category: {
-          id: -1,
-          name: '전체',
-          backColor: '#333333'
-        },
-        setCategory: jest.fn()
-      }
+      isActive: false
     },
     style: {
       backColor: '#333333'
@@ -25,10 +18,11 @@ const CATEGORIES = {
 
 describe('<CategoryButton>', () => {
   it('Button has text called "전체"', async () => {
-    render(
+    const { rerender } = render(
       <CategoryButton
         info={CATEGORIES['ALL'].info}
         style={CATEGORIES['ALL'].style}
+        onClick={jest.fn()}
       />
     );
 
@@ -36,12 +30,14 @@ describe('<CategoryButton>', () => {
     expect(categoryButton).toBeInTheDocument();
     expect(categoryButton).toHaveTextContent('전체 (7)');
     expect(categoryButton).toHaveStyle({
-      color: 'rgb(255, 255, 255)'
+      color: 'rgb(34, 34, 34)'
     });
     expect(categoryButton).toHaveStyle({
-      fontWeight: 'bold'
+      fontWeight: 'normal'
     });
-    expect(categoryButton).toHaveStyle({ backgroundColor: 'rgb(51, 51, 51)' });
+    expect(categoryButton).toHaveStyle({
+      backgroundColor: 'rgb(255, 255, 255)'
+    });
 
     await userEvent.hover(categoryButton);
     expect(categoryButton).toHaveStyle({ opacity: 0.7 });
@@ -49,6 +45,24 @@ describe('<CategoryButton>', () => {
     await userEvent.unhover(categoryButton);
     expect(categoryButton).toHaveStyle({
       opacity: 1
+    });
+
+    await userEvent.click(categoryButton);
+    rerender(
+      <CategoryButton
+        info={{ ...CATEGORIES['ALL'].info, isActive: true }}
+        style={CATEGORIES['ALL'].style}
+        onClick={jest.fn()}
+      />
+    );
+    expect(categoryButton).toHaveStyle({
+      color: 'rgb(255, 255, 255)'
+    });
+    expect(categoryButton).toHaveStyle({
+      fontWeight: 'bold'
+    });
+    expect(categoryButton).toHaveStyle({
+      backgroundColor: 'rgb(51, 51, 51)'
     });
   });
 });
