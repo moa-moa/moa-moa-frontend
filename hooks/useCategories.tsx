f06bf7f997ce75ef0a2c21478e07f716bd54e17a
taesung1993 <cheonyulin@gmail.com>
2022-10-03 20:58:21 +0900

    create and apply categoryStates using recoil to manage global state

--- hooks/useCategories.tsx
+++ hooks/useCategories.tsx
@@ -1,14 +1,21 @@
 import AuthService from '@/services/auth.service';
 import CategoriesService from '@/services/categories.service';
 import { useQuery } from '@tanstack/react-query';
+import { useSetRecoilState } from 'recoil';
+import { categoryStates } from 'store/categories';
 
 export default function useCategories() {
+  const setCategories = useSetRecoilState(categoryStates);
+
   const response = useQuery(['categories'], CategoriesService.getCategories, {
     retry: false,
     refetchOnWindowFocus: true,
     staleTime: 3000,
     keepPreviousData: true,
-    enabled: !!AuthService.accessToken
+    enabled: !!AuthService.accessToken,
+    onSuccess(data) {
+      setCategories([...data]);
+    }
   });
 
   return response;
