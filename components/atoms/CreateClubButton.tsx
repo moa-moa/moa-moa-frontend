205383ff882b541ae6b8766901b0f3ce840877cf
taesung1993 <cheonyulin@gmail.com>
2022-10-03 12:54:06 +0900

    add fragment called 'createClubModal' when user pressed '모임 등록' button

--- components/atoms/CreateClubButton.tsx
+++ components/atoms/CreateClubButton.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function CreateClubButton() {
   return (
     <div className='fixed bottom-[86px] right-[16px]'>
-      <Link href='/?cmd=create-club' as='/clubs/create'>
+      <Link href='/#createClubModal'>
         <a className='w-16 h-16 flex flex-col justify-center items-center rounded-full bg-[#ee2554] shadow-[0px_0px_20px_rgba(0, 0, 0, 0.3)]'>
           <Icons.Plus />
           <span className='text-[10px] leading-[15px] text-white'>
