8f683e656fd2fd8b50215859c881692c90eae58a
taesung1993 <cheonyulin@gmail.com>
2022-10-03 12:55:42 +0900

    add create-club-page-in-modal

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -2,12 +2,13 @@ import Layout from '@/components/templates';
 import Organisms from '../organisms';
 import Atoms from '../atoms';
 import Modal from '../atoms/Modal';
-import { useRouter } from 'next/router';
+import { Router, useRouter } from 'next/router';
 import useCategories from '@/hooks/useCategories';
 import { useState } from 'react';
+import Icons from '../icons';
 
 export default function Home() {
-  const router = useRouter();
+  const { asPath, push } = useRouter();
   const { data, isLoading, isError } = useCategories();
   const [selectedCategoryId, setSelectedCategoryId] = useState<number>(-1);
 
@@ -28,8 +29,26 @@ export default function Home() {
         <Organisms.UnAvailableClubs />
       </section>
       <Atoms.CreateClubButton />
-      <Modal isOpen={!!router.query.cmd}>
-        <h1>Hello</h1>
+      <Modal isOpen={asPath.split('#')[1] === 'createClubModal'}>
+        <section className='relative w-screen h-screen flex justify-center items-center'>
+          <section className='relative w-full max-w-xl h-auto max-h-full bg-white py-4 flex flex-col z-10'>
+            <header className='px-4 mb-5'>
+              <h1 className='text-xl font-bold'>모임 생성</h1>
+              <p className='text-sm'>모아모아 모임을 생성합니다.</p>
+              <button
+                className='absolute z-10 top-2 right-2'
+                onClick={() => push('/')}>
+                <Icons.Close />
+              </button>
+            </header>
+            <main className='overflow-y-auto'>
+              <Organisms.ClubForm isModal={true} />
+            </main>
+          </section>
+          <section
+            className='absolute top-0 left-0 w-screen h-screen bg-loading-bg z-0'
+            onClick={() => push('/')}></section>
+        </section>
       </Modal>
     </Layout.Nested>
   );
