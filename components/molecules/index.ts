d78db82aef9ccd837e769ad9d97bf1320dabfcf8
taesung1993 <cheonyulin@gmail.com>
2022-10-12 16:46:43 +0900

    remove clubList component from molecule

--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -1,11 +1,9 @@
 import ClubItem from './ClubItem';
-import ClubList from './ClubList';
 import Gnb from './Gnb';
 import SelectMaxPeople from './SelectMaxPeople';
 import UploadPhotos from './UploadPhotos';
 
 export default {
-  ClubList,
   ClubItem,
   Gnb,
   SelectMaxPeople,
