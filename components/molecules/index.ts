e4c258d52d5a6b12c5061844e99d7f245b51cbd5
taesung1993 <cheonyulin@gmail.com>
2022-10-13 15:34:56 +0900

    add and apply club detail header

--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -2,10 +2,12 @@ import ClubItem from './ClubItem';
 import Gnb from './Gnb';
 import SelectMaxPeople from './SelectMaxPeople';
 import UploadPhotos from './UploadPhotos';
+import DetailHeader from './DetailHeader';
 
 export default {
   ClubItem,
   Gnb,
   SelectMaxPeople,
-  UploadPhotos
+  UploadPhotos,
+  DetailHeader
 };
--- components/templates/ClubDetail.tsx
+++ components/templates/ClubDetail.tsx
@@ -1,50 +1,10 @@
 import { ILayoutProps } from '@/models/interfaces/props/layout';
-import { useMemo } from 'react';
-import { useRecoilValue } from 'recoil';
-import { categoryStates } from 'store/categories';
-import { clubDetailStates } from 'store/clubDetail';
-import { userState } from 'store/user';
-import Icons from '../icons';
+import Molecules from '../molecules';
 
 export default function ClubDetail({ children }: ILayoutProps) {
-  const categories = useRecoilValue(categoryStates);
-  const userInfo = useRecoilValue(userState);
-
-  const { categoryId, UserLikedClub, owner } =
-    useRecoilValue(clubDetailStates)!;
-
-  const category = useMemo(() => {
-    return categories.find((cate) => cate.id === categoryId);
-  }, [categoryId]);
-
-  const isLiked = useMemo(() => {
-    if (UserLikedClub.length === 0) {
-      return false;
-    }
-    return !!UserLikedClub.find((user) => user.userId === userInfo!.id);
-  }, [UserLikedClub, userInfo]);
-
-  const isOwner = useMemo(() => {
-    return userInfo!.id === owner;
-  }, [owner]);
-
   return (
     <section className='min-h-screen flex flex-col'>
-      <header className='sticky top-0 left-0 w-full h-header'>
-        <section className='max-w-5xl w-full h-full mx-auto relative'>
-          <button className='absolute top-1/2 -translate-y-1/2 left-0'>
-            <Icons.Back />
-          </button>
-          <h1 className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-            {category?.name || ''}
-          </h1>
-          {!isOwner && (
-            <button className='absolute top-1/2 -translate-y-1/2 right-0'>
-              {isLiked ? <Icons.LikeOn /> : <Icons.LikeOff />}
-            </button>
-          )}
-        </section>
-      </header>
+      <Molecules.DetailHeader />
       <main className='flex-1 mt-[0.1rem] max-w-5xl w-full mx-auto'>
         {children}
       </main>
