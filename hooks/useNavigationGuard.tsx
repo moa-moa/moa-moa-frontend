8daa316976270b78f52dac5bf73967f9d36ec803
taesung1993 <cheonyulin@gmail.com>
2022-10-13 14:59:17 +0900

    add geting user when successed login

--- hooks/useNavigationGuard.tsx
+++ hooks/useNavigationGuard.tsx
@@ -3,18 +3,22 @@ import { useRouter } from 'next/router';
 import AuthService from '@/services/auth.service';
 import useToasts from './useToasts';
 import { useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { userState } from 'store/user';
 
 export default function useNavigationGuard() {
   const [first, setFirst] = useState(true);
   const router = useRouter();
+  const setUser = useSetRecoilState(userState);
   const { addToast } = useToasts();
   const response = useQuery(['auth'], AuthService.googleLogin, {
     retry: false,
     refetchOnWindowFocus: false,
     enabled: !AuthService.accessToken,
-    onSuccess: (data) => {
-      AuthService.setAccessToken(data.accessToken);
+    onSuccess: ({ tokenInfo, userInfo }) => {
+      AuthService.setAccessToken(tokenInfo.accessToken);
       AuthService.refreshLogin(response.refetch);
+      setUser(userInfo);
       if (first) {
         addToast('success', '모아모아에 접속하신 것을 환영합니다.');
         setFirst(false);
@@ -25,6 +29,7 @@ export default function useNavigationGuard() {
       if (status === 401) {
         if (AuthService.accessToken) {
           await AuthService.logout();
+          setUser(null);
         }
       }
       router.replace('/login');
