68cb2bbd4ff22afbe1dfe0b60bad564f06f587dc
taesung1993 <cheonyulin@gmail.com>
2022-10-14 17:57:28 +0900

    add animation to modal component

--- components/atoms/Modal.tsx
+++ components/atoms/Modal.tsx
@@ -3,15 +3,17 @@ import { ReactNode } from 'react';
 interface Props {
   children: ReactNode;
   isOpen: boolean;
+  className?: string;
 }
 
-export default function Modal({ children, isOpen }: Props) {
+export default function Modal({ children, isOpen, className }: Props) {
   if (!isOpen) {
     return <></>;
   }
 
   return (
-    <section className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[1000]'>
+    <section
+      className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[1000] ${className}`}>
       {children}
     </section>
   );
