9b61d8f304e1aeb9e139bbf60678941b4e268300
taesung1993 <cheonyulin@gmail.com>
2022-10-14 18:42:01 +0900

    add sequence id in toast, not index number

--- hooks/useToasts.tsx
+++ hooks/useToasts.tsx
@@ -2,18 +2,18 @@ import { ToastTypes } from '@/models/interfaces/UI/toast';
 import { useCallback, useRef } from 'react';
 import { useRecoilState } from 'recoil';
 import { toastList } from 'store/toasts';
+import { v4 as uuid4 } from 'uuid';
 
 export default function useToasts() {
   const [list, setList] = useRecoilState(toastList);
-  const currentId = useRef(1);
   const addToast = useCallback(
     (type: ToastTypes, message: string) => {
+      const sequence = uuid4();
       const newToast = {
-        id: `toast-${currentId.current}`,
+        id: `toast-${sequence}`,
         type,
         message
       };
-      currentId.current += 1;
       setList((prev) => [...prev, newToast]);
     },
     [list]
