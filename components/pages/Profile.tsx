397fb7ee4dc5c96aebde356ac831be7fea8991ff
taesung1993 <cheonyulin@gmail.com>
2022-10-26 19:45:33 +0900

    initially publish profile page

--- components/pages/Profile.tsx
+++ components/pages/Profile.tsx
@@ -1,9 +1,61 @@
 import Layout from '@/components/templates';
+import Atoms from '../atoms';
+import Icons from '../icons';
+import Link from 'next/link';
 
 export default function Profile() {
   return (
     <Layout.Nested>
-      <h1 className='text-3xl underline'>Profile</h1>
+      <section className='flex flex-col justify-center items-center gap-2.5 mt-[1.875rem]'>
+        <div className='relative'>
+          <Atoms.Avatar name='Mason' size={111} isAvailable={true} />
+          <div className='absolute right-0 bottom-0 w-[2.375rem] h-[2.375rem]'>
+            <button className='w-full h-full'>
+              <Icons.Camera />
+            </button>
+          </div>
+        </div>
+        <h6 className='font-bold text-[1.25rem] leading-[1.875rem] text-[#222] -tracking-[0.01rem]'>
+          윤태성
+        </h6>
+      </section>
+      <section className='mt-[1.875rem]'>
+        <ul className='flex flex-col items-center justify-center gap-2.5'>
+          <li className='w-full h-16 max-w-[19.4375rem] border border-border-gray rounded-[0.3125rem]'>
+            <Link href={'/'}>
+              <a className='flex items-center w-full p-5'>
+                <Icons.MyCrown />
+                <span className='flex-1 pl-3 text-[0.9375rem] leading-[1.375rem] text-[#222] -tracking-[0.01rem]'>
+                  내가 만든 모임
+                </span>
+                <Icons.Rarrow />
+              </a>
+            </Link>
+          </li>
+          <li className='w-full h-16 max-w-[19.4375rem] border border-border-gray rounded-[0.3125rem]'>
+            <Link href={'/'}>
+              <a className='flex items-center w-full p-5'>
+                <Icons.MyJoin />
+                <span className='flex-1 pl-3 text-[0.9375rem] leading-[1.375rem] text-[#222] -tracking-[0.01rem]'>
+                  참여한 모임
+                </span>
+                <Icons.Rarrow />
+              </a>
+            </Link>
+          </li>
+          <li className='w-full h-16 max-w-[19.4375rem] border border-border-gray rounded-[0.3125rem]'>
+            <Link href={'/'}>
+              <a className='flex items-center w-full p-5'>
+                <Icons.MyLike />
+                <span className='flex-1 pl-3 text-[0.9375rem] leading-[1.375rem] text-[#222] -tracking-[0.01rem]'>
+                  찜한 모임
+                </span>
+                <Icons.Rarrow />
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </section>
     </Layout.Nested>
   );
 }
